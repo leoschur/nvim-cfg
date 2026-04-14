5d208a760892295b2c46ec9ec0e50a43a03bfe4d
Leo Schurrer <leo.schurrer@pm.me>
2026-04-14 21:09:02 +0200

    Cpp dev setup

---
+++ lua/configs/dap.lua
@@ -0,0 +1,11 @@
+local dap = require("dap")
+
+dap.adapters.codelldb = {
+  type = "server",
+  port = "${port}",
+  executable = {
+    command = vim.fn.stdpath("data") .. "/mason/bin/codelldb",
+    args = { "--args", "${port}" },
+  }
+}
+
--- lua/mappings.lua
+++ lua/mappings.lua
@@ -8,3 +8,31 @@ map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+
+map("n", "<F5>", function()
+  require("dap").continue()
+end)
+map("n", "<F10>", function()
+  require("dap").step_over()
+end)
+map("n", "<F11>", function()
+  require("dap").step_into()
+end)
+map("n", "<F12>", function()
+  require("dap").step_out()
+end)
+map("n", "<leader>b", function()
+  require("dap").toggle_breakpoint()
+end)
+
+-- -- Normal mode
+-- map("n", "<C-Up>", ":m .-2<CR>==", { desc = "Move line up" })
+-- map("n", "<C-Down>", ":m .+1<CR>==", { desc = "Move line down" })
+--
+-- Insert mode
+-- map("i", "<C-Up>", "<Esc>:m .-2<CR>==gi", { desc = "Move line up" })
+-- map("i", "<C-Down>", "<Esc>:m .+1<CR>==gi", { desc = "Move line down" })
+
+-- Visual mode (for selected blocks)
+-- map("v", "<C-Up>", ":m '<-2<CR>gv=gv", { desc = "Move selection up" })
+-- map("v", "<C-Down>", ":m '>+1<CR>gv=gv", { desc = "Move selection down" })
